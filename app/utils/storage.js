e20e60bb68d1c83fa2305759b6e0c703db2b33b6
Evan Johnson <evanrjohnso@gmail.com>
2020-12-29 19:20:54 -0800

    finally remove that badge

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -5,13 +5,13 @@ function saveState(state) {
 // todos unmarked count
 function setBadge(todos) {
   if (chrome.browserAction) {
-    const count = todos.filter(todo => !todo.marked).length;
-    chrome.browserAction.setBadgeText({ text: count > 0 ? count.toString() : '' });
+    const count = todos.filter((todo) => !todo.marked).length;
+    // chrome.browserAction.setBadgeText({ text: count > 0 ? count.toString() : '' });
   }
 }
 
 export default function () {
-  return next => (reducer, initialState) => {
+  return (next) => (reducer, initialState) => {
     const store = next(reducer, initialState);
     store.subscribe(() => {
       const state = store.getState();
